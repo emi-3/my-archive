7b236f867c6dc3e2aef969ce7edfcf8a39dd9f87
emi-3 <works.mail.emi@gmail.com>
2019-09-09 21:06:07 +0900

    initial commit

---
+++ z-index_vs_important/style.js
@@ -0,0 +1,9 @@
+$(function(){
+  $('#box05').css({'cssText': 'z-index: 9999 !important;'});  
+});
+$(function(){
+  $('#box06').css('z-index','9999');
+});
+$(function(){
+  $('#box07').css({'cssText': 'z-index: 888 !important;'});
+});
